bdcc81b7c3d21ff7df32672bac0f7e34385afb44
김유진 <yj.kim@itforest.net>
2024-06-04 16:40:58 +0900

    page scroll added (without main page)

--- app.py
+++ app.py
@@ -3,84 +3,45 @@ import mysql.connector
 
 app = Flask(__name__)
 
-# MySQL 연결 설정
-def get_db_connection():
-    return mysql.connector.connect(
+def query_database(query, args=(), one=False):
+    conn = mysql.connector.connect(
         host="localhost",
         user="root",
         password="root",
         database="searchdb"
     )
-
-def data_fining(data):
-    result = []
-    for line in data:
-        tmp = []
-        if line[0] == "G": tmp.append("Google")
-        else: tmp.append("Bing")
-
-        if ":" in line[1]:
-            str = line[1].split(':')[0]
-            tmp.append(str)
-        else: tmp.append(line[1])
-
-        for i in range(2, 5): tmp.append(line[i])
-        result.append(tmp)
-    return result
+    cur = conn.cursor()
+    cur.execute(query, args)
+    r = cur.fetchall()
+    cur.close()
+    conn.close()
+    return (r[0] if r else None) if one else r
 
 @app.route('/')
 def index():
-    db_connection = get_db_connection()
-    cur = db_connection.cursor()
+    count = query_database('SELECT COUNT(*) FROM searchresult')
+    data = query_database('SELECT * FROM searchresult LIMIT 20')
+    return render_template('content.html', count=count, data=data)
 
-    cur.execute("SELECT * FROM searchresult")
-    data = cur.fetchall()
-    data_list = data_fining(data)
-
-    cur.execute("SELECT count(*) FROM searchresult")
-    count = cur.fetchall()
-
-    cur.close()
-    db_connection.close()
-
-    return render_template('content.html', data=data_list, count=count)
-
-@app.route('/list')
-def search_list():
-    return render_template('list.html')
-
-@app.route('/parses')
-def parsed_data():
-    return render_template('parses.html')
-
-@app.route('/search/')
-def search_result():
+@app.route('/search/', methods=['GET'])
+def search():
     menu = request.args.get('menu', '')
     key = request.args.get('key', '')
+    page = int(request.args.get('page', 1))
+    per_page = 20
+    offset = (page - 1) * per_page
+    
+    if menu and key:
+        query = f"SELECT COUNT(*) FROM searchresult WHERE {menu} LIKE %s"
+        count = query_database(query, (f'%{key}%',))
+        
+        query = f"SELECT * FROM searchresult WHERE {menu} LIKE %s LIMIT %s OFFSET %s"
+        data_list = query_database(query, (f'%{key}%', per_page, offset))
+    else:
+        count = [(0,)]
+        data_list = []
 
-    if menu not in ['subdomain', 'title', 'url', 'content']:
-        return "Invalid search field", 400
-
-    db_connection = get_db_connection()
-    cur = db_connection.cursor()
-
-    query = f"SELECT * FROM searchresult WHERE {menu} LIKE %s"
-    cur.execute(query, ('%' + key + '%',))
-    data = cur.fetchall()
-    data_list = data_fining(data)
-
-    query = f"SELECT count(*) FROM searchresult WHERE {menu} LIKE %s"
-    cur.execute(query, ('%' + key + '%',))
-    count = cur.fetchall()
-
-    result = {
-        "data_list": data_list,
-        "count": count
-    }
-    return jsonify(result)
-
-######## APP.RUN ########
+    return jsonify(count=count, data_list=data_list)
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0')
     app.run(debug=True)
